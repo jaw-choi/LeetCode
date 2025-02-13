ad06947e7cb94bc1f1afe0a4da7a9a09e5b47eae
jaewoo.choi <wod6983@gmail.com>
2025-02-13 22:18:17 +0900

    Time: 0 ms (100%), Space: 17 MB (81.82%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,26 +4,11 @@ public:
         int idx = 0;
         int left = 0;
         int ans = 0;
-        for(int i=0;i<prices.size();i++)
+        for(int i=1;i<prices.size();i++)
         {
-            if(prices[left] >= prices[i])            
-                left = i;            
-            else
-            {
-                if(i+1 < prices.size())
-                {
-                    if(prices[i]>=prices[i+1]){
-                        ans += prices[i]-prices[left];
-                        left = i+1;
-                    }
-                }
-                else
-                {
-                    if(prices[i]-prices[left]>0)
-                        ans+=prices[i]-prices[left];
-                    
-                }
-            }
+            int price = prices[i] - prices[i-1];
+            if(price>0)
+                ans+=price;
         }
         return ans;
     }
