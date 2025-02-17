2f1ca21ce6adc6411d866b9b0a2f993f170b8885
jaewoo.choi <wod6983@gmail.com>
2025-02-17 11:33:29 +0900

    Time: 3 ms (38.19%), Space: 112.3 MB (72.71%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    bool circular(int idx,int curr,int cnt, vector<int>& gas, vector<int>& cost)
-    {
-        if(cnt == gas.size())
-            return true;
-        if(curr+gas[idx] - cost[idx] < 0)
-            return false;
-        else
-        {
-            if(idx+1>=gas.size())
-                return circular(0,curr+gas[idx] - cost[idx],cnt+1,gas,cost);
-            else
-                return circular(idx+1,curr+gas[idx] - cost[idx],cnt+1,gas,cost);
-        }
-        return false;
-    }
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        if(gas.size()==1 && gas[0] == cost[0])
-            return 0;
+        int gasSum = accumulate(gas.begin(),gas.end(),0);
+        int costSum = accumulate(cost.begin(),cost.end(),0);
+        int ans = 0;
+        if(gasSum < costSum)
+            return -1;
+        int sum=0;
         for(int i=0;i<gas.size();i++)
         {
-            if(gas[i] <= cost[i])
-                continue;
-            if(circular(i,0,0,gas,cost))
-                return i;
+            sum += gas[i] - cost[i];
+            if(sum < 0)
+            {
+                sum = 0;
+                ans = i+1;
+            }
         }
-        return -1;
+        return ans;
     }
 };
