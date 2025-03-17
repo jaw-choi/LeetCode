eb13a3a2e2d2b5d294d24e89f73c3f386de71708
jaewoo.choi <wod6983@gmail.com>
2025-03-17 21:27:22 +0900

    Time: 76 ms (18.54%), Space: 59.3 MB (45.45%) - LeetHub

---
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool timeStuff(int cars, const vector<int>& ranks, long long time)
+    {
+        long long carDone = 0;
+        for(auto r : ranks)
+        {
+            long long c2 = time / r;
+            long long c = floor(sqrt(c2));
+            carDone += c;
+        }
+        return carDone >= cars;
+    }
+    long long repairCars(vector<int>& ranks, int cars) {
+        sort(ranks.begin(),ranks.end());
+        long long ans;
+        long long left = 1;
+        long long right = 1e14;
+        while(left<=right)
+        {
+            long long mid = (left+right)/2;
+
+            if(timeStuff(cars,ranks,mid))
+            {
+                ans = mid;
+                right = mid - 1;
+            }
+            else
+            {
+                left  = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
