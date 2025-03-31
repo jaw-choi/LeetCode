1fcbf44445210b12a90a0423402359d0028dbd86
jaewoo.choi <wod6983@gmail.com>
2025-03-31 13:42:45 +0900

    Time: 73 ms (15.39%), Space: 21.6 MB (14.7%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -18,7 +18,13 @@ public:
         for(int i=0;i<cnt.size();i++)
         {
             helper(i,cnt[i],nums,cnt);
+            if(cnt[cnt.size()-1]!=INT_MAX)
+                return cnt[cnt.size()-1];
         }
         return cnt[cnt.size()-1];
     }
 };
+//[2,3,1,1,4]
+//[0,-,-,-,-]
+//[0,1,1,-,-]
+//[0,1,2,2,2]
