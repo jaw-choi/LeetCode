2b720f8986bed8e4372b77e2e9b9f05aab5af346
jaewoo.choi <wod6983@gmail.com>
2025-04-08 12:11:30 +0900

    Time: 388 ms (5.8%), Space: 40.9 MB (5.63%) - LeetHub

---
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        //1,2,2,3,3,3,4,5,7
+        //1,2,3,4,2,3,3,5,7
+        //4,2,3,3,5,7
+        //3,5,7
+        //5,9,3,2,1,3
+
+        int answer = 0;
+        for(int i=0;i<nums.size();i+=3)
+        {
+            set<int> s(nums.begin()+i,nums.end());
+            if(s.size()==nums.size()-i)
+                return answer;
+            for(auto ss:s)
+                cout << ss << " ";
+            cout <<endl;
+            answer++;
+        }
+        return answer;
+/*
+bitset<51> bit(1);
+            for(int j=i;j<nums.size();j++)
+            {
+                if((bit&bit<<nums[j])!=0){
+                    cout <<endl;
+                    break;
+                }
+                bit |= bit<<nums[j];
+                cout << bit.to_string() << " "<< nums[j]<< endl;
+                if(j==nums.size()-1)
+                    return answer;
+            }
+            answer++;
+*/
+    }
+};
