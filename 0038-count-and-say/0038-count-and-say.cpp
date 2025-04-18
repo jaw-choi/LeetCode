c2ed45e1203e89975e5be3c4e67079225d91bd67
jaewoo.choi <wod6983@gmail.com>
2025-04-18 10:46:21 +0900

    Time: 4 ms (56.36%), Space: 11.2 MB (22.35%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -3,19 +3,21 @@ public:
     string countAndSay(int n) {
         vector<string> dp(n+1,"");
         dp[1]="1";
+        int cnt = 0;
         for(int i=2;i<=n;i++)
         {
-            for(int j=0;j<dp[i-1].size();)
+            string& prev = dp[i-1];
+            string curr;
+            for(int j=0,start=0;j<=prev.size();++j)
             {
-                char num = dp[i-1][j];
-                int cnt = 0;
-                while(j<dp[i-1].size()&&dp[i-1][j]==num){
-                    j++;
-                    cnt++;
+                if(j==prev.size()||prev[j]!=prev[start])
+                {
+                    int cnt = j-start;
+                    curr += to_string(cnt)+prev[start];
+                    start = j;
                 }
-                dp[i]+= to_string(cnt)+num;
-
             }
+            dp[i]=curr;
         }
         return dp[n];        
     }
