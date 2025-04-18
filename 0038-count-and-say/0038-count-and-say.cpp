e1bb3b9974262040b8f1a1d9be44717f77a37a51
jaewoo.choi <wod6983@gmail.com>
2025-04-18 10:44:35 +0900

    Time: 7 ms (25.49%), Space: 10.9 MB (24.61%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -5,17 +5,14 @@ public:
         dp[1]="1";
         for(int i=2;i<=n;i++)
         {
-            int idx = 0;
             for(int j=0;j<dp[i-1].size();)
             {
-                char num = dp[i-1][idx];
+                char num = dp[i-1][j];
                 int cnt = 0;
                 while(j<dp[i-1].size()&&dp[i-1][j]==num){
                     j++;
                     cnt++;
                 }
-                if(j<dp[i-1].size())
-                    idx = j;
                 dp[i]+= to_string(cnt)+num;
 
             }
