ab50fd2775f66a3ca0fad4293eb86be564b2dacc
jaewoo.choi <wod6983@gmail.com>
2025-05-13 19:17:45 +0900

    Time: 31 ms (95.07%), Space: 19.3 MB (70.94%) - LeetHub

--- 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
@@ -3,20 +3,22 @@ public:
     int lengthAfterTransformations(string s, int t) {
         constexpr int mod = 1e9 + 7;
         int ans = 0;
-        vector<int> cnt(26);
+        int cnt[26];
+        //vector<int> cnt(26);
         for(auto ch : s)
             cnt[ch-'a']++;
 
         for(int i=0;i<t;i++)
         {
-            vector<int> nxt(26);
+            int nxt[26]={};
             nxt[0] = cnt[25];
             nxt[1] = (cnt[25]+cnt[0])%mod;
             for(int j=2;j<26;j++)
             {
                 nxt[j] = cnt[j-1];
             }
-            cnt = move(nxt);
+            // cnt = move(nxt);
+            copy(nxt, nxt + 26, cnt);
         }
         for(auto c : cnt)
             ans = (ans+c)%mod;
