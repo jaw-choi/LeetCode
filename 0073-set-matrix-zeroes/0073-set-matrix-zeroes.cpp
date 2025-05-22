df1864a7771f59b092e11bfba0e127362d997546
jaewoo.choi <wod6983@gmail.com>
2025-05-22 23:11:37 +0900

    Time: 0 ms (100%), Space: 18.5 MB (78.87%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        bool zeroFirstCol = false;
+        for (int i = 0; i < matrix.size(); i++) {
+            if (matrix[i][0] == 0)
+                zeroFirstCol = true;
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = matrix.size()-1; i >= 0; i--) {
+            for (int j = matrix[0].size()-1; j >=1; j--) {
+                if(matrix[i][0] == 0 || matrix[0][j]==0)
+                    matrix[i][j]=0;
+            }
+            if(zeroFirstCol)
+                matrix[i][0]=0;
+        }
+    }
+};
+
+/*
         unordered_set<int> xpos;
         unordered_set<int> ypos;
         for(int i=0;i<matrix.size();i++)
@@ -23,4 +47,4 @@ public:
         }
 
     }
-};
+*/
