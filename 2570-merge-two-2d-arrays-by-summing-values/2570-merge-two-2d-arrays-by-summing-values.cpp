d36b18396632f997130085a79b9911edf23196f6
jaewoo.choi <wod6983@gmail.com>
2025-05-23 14:29:00 +0900

    Time: 3 ms (31.88%), Space: 14.7 MB (63.04%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -3,10 +3,11 @@ public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
         int currId = 1;
         int i=0,j=0;
+        int sum = 0;
         vector<vector<int>> ans;
         while(i<nums1.size() || j<nums2.size())
         {
-            int sum = 0;
+            sum = 0;
             if(i<nums1.size()&&nums1[i][0]==currId){
                 sum+=nums1[i][1];
                 i++;
