2d8c891f464f9e38b3cead8046557da8c7760415
jaewoo.choi <wod6983@gmail.com>
2025-06-05 14:35:48 +0900

    Time: 11 ms (23.19%), Space: 11.5 MB (15.96%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    // DFS to find the smallest lex character in the component
-    char dfs(unordered_map<char, vector<char>>& adj, char cur, vector<int>& vis) {
-        vis[cur - 'a'] = 1;
-        char minChar = cur;
-        for (char neighbor : adj[cur]) {
-            if (vis[neighbor - 'a'] == 0) {
-                minChar = min(minChar, dfs(adj, neighbor, vis));
+    char dfs(unordered_map<char,vector<char>>& mp, char ch, vector<int>& visited)
+    {
+        visited[ch - 'a'] = 1;
+        char minChar = ch;
+        for(auto n : mp[ch])
+        {
+            if(visited[n - 'a']==0){
+                minChar = min(minChar,dfs(mp,n,visited));
             }
         }
         return minChar;
     }
-
     string smallestEquivalentString(string s1, string s2, string baseStr) {
         int n = s1.length();
-        unordered_map<char, vector<char>> adj;
+        unordered_map<char,vector<char>> mp;
 
-        // Step 1: Build the equivalence graph
-        for (int i = 0; i < n; ++i) {
-            char u = s1[i];
-            char v = s2[i];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        for(int i=0;i<n;i++)
+        {
+            mp[s1[i]].push_back(s2[i]);
+            mp[s2[i]].push_back(s1[i]);
         }
 
-        // Step 2: Replace each character in baseStr with the smallest equivalent
-        string result;
-        for (char ch : baseStr) {
-            vector<int> vis(26, 0);
-            char minChar = dfs(adj, ch, vis);
-            result.push_back(minChar);
+        string ans = "";
+        for(auto ch : baseStr)
+        {
+            vector<int> visited(26,0);
+            char minChar = dfs(mp,ch,visited);
+            ans += minChar;
         }
-
-        return result;
+        return ans;
     }
 };
