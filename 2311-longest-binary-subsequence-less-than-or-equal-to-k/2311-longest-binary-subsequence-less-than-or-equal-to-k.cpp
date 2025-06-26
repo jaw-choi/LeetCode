5d0247972ca7232e3b3bd3cb098ca256906eb417
jaewoo.choi <wod6983@gmail.com>
2025-06-26 09:42:07 +0900

    Time: 0 ms (100%), Space: 9.4 MB (96.83%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -4,17 +4,15 @@ public:
         int curr = 1;
         int num = 0;
         int cnt = 0;
-        cout << s.length()<<endl;
+
         for(auto it=s.rbegin();it<s.rend();++it)
         {
             if(*it=='1' && (num+curr <= k)){
                 num += curr;
                 cnt++;
-                //cout <<*it;
             }
             else if(*it=='0'){
                 cnt++;
-                //cout <<*it;
             }
             if(curr <= k)
                 curr*=2;
