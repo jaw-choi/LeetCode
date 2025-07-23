47d4c272a5f1a1e7dffa13024ca99885266bf83a
jaewoo.choi <wod6983@gmail.com>
2025-07-23 10:28:15 +0900

    Time: 617 ms (5.14%), Space: 19.9 MB (92.93%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int removeStr(string& str, const string& s) {
+        int cnt = 0;
+        for (int i = 0; i < (int)str.size() - 1; i++) {
+            if (str[i] == s[0] && str[i + 1] == s[1]) {
+                str.erase(i, 2);
+                cnt++;
+                i = max(i - 2, -1); // -1로 두는 이유는 다음 반복에서 i++ 되어 0이 되도록
+            }
+        }
+        return cnt;
+    }
+
+    int maximumGain(string s, int x, int y) {
+        string strX = "ab";
+        string strY = "ba";
+        int ans = 0;
+
+        if (x >= y) {
+            ans += x * removeStr(s, strX);
+            ans += y * removeStr(s, strY);
+        } else {
+            ans += y * removeStr(s, strY);
+            ans += x * removeStr(s, strX);
+        }
+
+        return ans;
+    }
+};
