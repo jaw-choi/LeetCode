175fc0598772b03f8274f92db8872b8d6ba7e504
jaewoo.choi <wod6983@gmail.com>
2025-08-12 10:47:07 +0900

    Time: 19 ms (87.36%), Space: 12.9 MB (71.9%) - LeetHub

---
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    static const int MOD = 1'000'000'007;
+
+    long long ipow(long long a, int x) {
+        long long r = 1;
+        while (x) {
+            if (x & 1) r *= a;
+            a *= a;
+            x >>= 1;
+        }
+        return r;
+    }
+
+public:
+    int numberOfWays(int n, int x) {
+        vector<int> powers;
+        for (long long i = 1;; ++i) {
+            long long p = ipow(i, x);
+            if (p > n) break;
+            powers.push_back((int)p);
+        }
+
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int p : powers) {
+            for (int s = n; s >= p; --s) {
+                dp[s] += dp[s - p];
+                if (dp[s] >= MOD) dp[s] -= MOD;
+            }
+        }
+        return dp[n];
+    }
+};
