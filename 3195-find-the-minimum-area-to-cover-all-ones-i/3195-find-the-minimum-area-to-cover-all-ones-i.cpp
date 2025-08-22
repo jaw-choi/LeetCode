076afaa238b8fce3ea5e4cc79ec7edf2df0814cf
jaewoo.choi <wod6983@gmail.com>
2025-08-22 10:23:43 +0900

    Time: 255 ms (66.45%), Space: 133.4 MB (72.52%) - LeetHub

--- 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -3,38 +3,39 @@ public:
     int minimumArea(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        bool flag = false;
-        int minGrid = 0;
-        int minN = 0;
-        int minM = 0;
-        int maxGridN = 0;
-        int maxGridM = 0;
+        pair<int,int> minJ(INT_MAX,INT_MAX);
+        pair<int,int> minI(INT_MAX,INT_MAX);
+        pair<int,int> maxI(0,0);
+        pair<int,int> maxJ(0,0);
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==1){
-                    if(!flag){
-                        flag = true;
-                        minGrid = min(i,j);
-                        minN = i;
-                        minM = j;
+                    if(minJ.second > j){
+                        minJ = make_pair(i,j);
                     }
-                    maxGridN = i;
-                    maxGridM = j;
+                    if(minI.first > i){
+                        minI = make_pair(i,j);
+                    }
+                    if(maxJ.second < j){
+                        maxJ = make_pair(i,j);
+                    }
+                    if(maxI.first < i){
+                        maxI = make_pair(i,j);
+                    }
                 }
-                
             }
         }
-        cout << minGrid << " " << minGrid << " " << maxGridN << " " << maxGridM <<endl;
-        if(maxGridN==0 || maxGridM ==0){
-            if(grid[minGrid][minGrid]==0){
-            if(maxGridN!=0)
-                return maxGridN;
-            if(maxGridM!=0)
-                return maxGridM;
-            }
-        }
-        return (maxGridN-minGrid+1)*(maxGridM-minGrid+1);
+        cout << minJ.first <<"," << minJ.second<<endl;
+        cout << minI.first <<"," << minI.second<<endl;
+        cout << maxJ.first <<"," << maxJ.second<<endl;
+        cout << maxI.first <<"," << maxI.second<<endl;
+        int minX = min(minJ.first,minI.first); 
+        int minY = min(minJ.second,minI.second); 
+        int maxX = max(maxJ.first,maxI.first); 
+        int maxY = max(maxJ.second,maxI.second); 
+        cout << minX << " " << minY << " "<< maxX << " " << maxY<<endl;
+        return (maxX - minX + 1) * (maxY - minY + 1);
     }
 };
