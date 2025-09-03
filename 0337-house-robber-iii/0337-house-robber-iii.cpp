c72d9c29cfd3de7cac1b4a3750e1b008e7f7f878
jaewoo.choi <wod6983@gmail.com>
2025-09-03 09:31:42 +0900

    Time: 0 ms (100%), Space: 22.4 MB (92.39%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -9,21 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
-
 public:
     pair<int,int> robSub(TreeNode* root)
     {
-        if(!root) return {0,0}; 
-        auto left = robSub(root->left);
-        auto right = robSub(root->right);
-        int skip = max(left.first,left.second) + max(right.first,right.second);
-        int take = root->val + left.first + right.first;
+        if(!root) return {0,0};
+        auto l = robSub(root->left);
+        auto r = robSub(root->right);
+        int skip = max(l.first,l.second) + max(r.first,r.second);
+        int take = root->val + l.first + r.first;
         return {skip,take};
     }
-    int rob(TreeNode* root){
-        pair<int,int> result = robSub(root);
+    int rob(TreeNode* root) {
+        pair<int,int> result;
+        result = robSub(root);
         return max(result.first,result.second);
     }
 };
