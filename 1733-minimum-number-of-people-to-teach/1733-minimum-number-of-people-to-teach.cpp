4f690761dc137f244fdb3009ff20d4598dd1e8e6
jaewoo.choi <wod6983@gmail.com>
2025-09-10 10:48:19 +0900

    Time: 53 ms (72.94%), Space: 64.8 MB (88.24%) - LeetHub

---
+++ 1733-minimum-number-of-people-to-teach/1733-minimum-number-of-people-to-teach.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool diff(vector<int>& a, vector<int>& b,set<int>& s)
+    {
+        for(int i=0;i<a.size();i++)
+        {
+            for(int j=0;j<b.size();j++)
+            {
+                if(a[i]==b[j])
+                    return false;
+                
+            }
+        }
+        return true;
+    }
+    void helper(vector<int>& connect, vector<vector<int>>& languages, set<int>& s )
+    {
+        if(diff(languages[connect[0]-1],languages[connect[1]-1],s))
+        {
+            s.insert(connect[0]-1);
+            s.insert(connect[1]-1);
+        }
+    }
+    int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
+        set<int> s;
+        for(int i=0;i<friendships.size();i++)
+        {
+            helper(friendships[i],languages,s);
+        }
+        map<int,int> mp;
+        int maxNum = 0;
+        for(auto n : s)
+        {
+            auto v = languages[n];
+            for(int i=0;i<v.size();i++)
+            {
+                mp[v[i]]++;
+                if(mp[v[i]] > maxNum)
+                    maxNum = mp[v[i]];
+            }
+        }
+        int result = s.size() - maxNum;
+        return result;
+    }
+};
