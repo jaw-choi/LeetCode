29f580d4592fd7fc8dafa41312559d6c0e7001d4
jaewoo.choi <wod6983@gmail.com>
2025-09-11 08:32:15 +0900

    Time: 27 ms (30.91%), Space: 61 MB (36.17%) - LeetHub

--- 2560-house-robber-iv/2560-house-robber-iv.cpp
+++ 2560-house-robber-iv/2560-house-robber-iv.cpp
@@ -2,28 +2,27 @@ class Solution {
 public:
     int minCapability(vector<int>& nums, int k) {
         int left = 1;
-        int right = INT_MAX;
-        int cnt = 0;
+        int right = *max_element(nums.begin(),nums.end());
+        int n = nums.size();
         int ans = 0;
-
         while(left<=right)
         {
             int mid = (right-left)/2 + left;
-            cnt=0;
-            for(int i=0;i<nums.size();i++)
+            int cnt = 0;
+            for(int i=0;i<n;i++)
             {
                 if(nums[i] <= mid){
                     cnt++;
                     i++;
                 }
             }
+
             if(cnt >= k){
                 ans = mid;
-                right = mid-1;
+                right = mid - 1;
             }
-            else{
+            else
                 left = mid+1;
-            }
         }
         return ans;
     }
