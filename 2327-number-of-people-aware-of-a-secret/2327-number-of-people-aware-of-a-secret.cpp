aa0c9676398382ccd846b292619f240db0b531fb
jaewoo.choi <wod6983@gmail.com>
2025-09-15 13:38:09 +0900

    Time: 0 ms (100%), Space: 9.4 MB (47.37%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int mod = 1e9 + 7;
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        int ans = 0;
+        int mod = 1e9+7;
+        long long answer = 0;
         vector<int> dp(n+1,0);
         dp[1]=1;
         long long share = 0;
-        for(int day = 2;day<=n;day++)
+        for(int today=2;today<=n;today++)
         {
-            if(day-delay>0)//share
+            if(today - delay >0)
             {
-                share = (share + dp[day-delay]+mod)%mod;
+                share = (share + dp[today - delay] + mod) % mod;
             }
-            if(day-forget > 0)//forget
+            if(today - forget > 0)
             {
-                share = (share - dp[day-forget]+mod)%mod;
+                share = (share - dp[today - forget] + mod)%mod;
             }
-            dp[day] = share;
+            //cout << share <<endl;
+            dp[today] = share;
         }
-        long long know = 0;
-        for(int i = n - forget + 1;i<=n;i++)
-        {
-            know = (know + dp[i])%mod;
+        for(int i=n-forget+1;i<=n;i++){
+            answer = (answer + dp[i])%mod;
+            //cout << i<<" " << dp[i] << " " << answer <<endl;
         }
-        return know;
+
+        return answer;
     }
 };
