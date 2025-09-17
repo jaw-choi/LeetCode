6eca201167e15e5fa9f05dbaae3fc182811db930
jaewoo.choi <wod6983@gmail.com>
2025-09-17 12:55:10 +0900

    Time: 0 ms (100%), Space: 136.6 MB (7.37%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
+    bool isNCP(int a, int b) { return getGCM(a, b) > 1; }
+    int getGCM(int a, int b) {
+        if (b == 0)
+            return a;
+        return getGCM(b, a % b);
+    }
+    int getLCM(int a, int b) { return ((long long)a * b) / getGCM(a, b); }
     vector<int> replaceNonCoprimes(vector<int>& nums) {
-        vector<int> ans;
-        int n = nums.size();
+        vector<int> answer;
         stack<int> st;
-        for (auto& x : nums) {
+        int n = nums.size();
+        int idx = 0;
+        for(auto& x : nums) {
             st.push(x);
 
-            while (st.size() > 1) {
-                int left = st.top();
-                st.pop();
-                int right = st.top();
-                st.pop();
-
-                if (gcd(left, right) > 1) {
-                    st.push(lcm(left, right));
+            while(st.size() >= 2) {
+                int right = st.top(); st.pop();
+                int left = st.top(); st.pop();
+                if (gcd(left,right)!=1) {
+                    st.push(lcm(left,right));
+                    // cout << LCM << " " << left << " " << right << endl;
                 } else {
-                    st.push(right);
                     st.push(left);
+                    st.push(right);
                     break;
                 }
             }
-        }
 
-        while (!st.empty()) {
+        }
+        while(!st.empty()) {
             int num = st.top();
             st.pop();
-            ans.push_back(num);
+            answer.push_back(num);
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        reverse(answer.begin(),answer.end());
+        return answer;
     }
 };
+auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
