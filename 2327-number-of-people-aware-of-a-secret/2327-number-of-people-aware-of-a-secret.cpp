655af9bb1cde9f4d5785a9b2dbb9d9d838bc65b7
jaewoo.choi <wod6983@gmail.com>
2025-09-17 12:00:48 +0900

    Time: 1 ms (58.41%), Space: 9.4 MB (47.39%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int peopleAwareOfSecret(int n, int delay, int forget) {
-        int mod = 1e9+7;
-        long long answer = 0;
-        vector<int> dp(n+1,0);
-        dp[1]=1;
+        int mod = 1e9 + 7;
         long long share = 0;
-        for(int today=2;today<=n;today++)
+        vector<int> dp(n+1,0);
+        long long answer = 0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
         {
-            if(today - delay >0)
+            if(i-delay>0)
             {
-                share = (share + dp[today - delay] + mod) % mod;
-           // cout << "+"<<today << " : " << share << " " << dp[today-delay]<<endl;
+                share = (share + dp[i-delay]) % mod;
             }
-            if(today - forget > 0)
+            if(i-forget>0)
             {
-                share = (share - dp[today - forget] + mod)%mod;
-            //cout << "-"<<today << " : " << share << " " << dp[today-forget]<<endl;
+                share = (share - dp[i-forget] + mod) % mod;
             }
-            dp[today] = share;
-            //cout <<"dp"<<dp[today]<<endl;
+            dp[i] = share;
         }
-        for(int i=n-forget+1;i<=n;i++){
-            answer = (answer + dp[i])%mod;
+        for(int i = n - forget + 1;i<=n;i++)
+        {
+            answer = (answer + dp[i]) % mod;
         }
-
         return answer;
     }
 };
