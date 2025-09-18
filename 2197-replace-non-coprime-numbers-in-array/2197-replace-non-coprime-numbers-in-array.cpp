35cab81c9369bfd1b22952ee7dab7870ce9e86e5
jaewoo.choi <wod6983@gmail.com>
2025-09-18 16:04:28 +0900

    Time: 47 ms (9.17%), Space: 136.5 MB (5.38%) - LeetHub

--- 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
+++ 2197-replace-non-coprime-numbers-in-array/2197-replace-non-coprime-numbers-in-array.cpp
@@ -2,34 +2,32 @@ class Solution {
 public:
     vector<int> replaceNonCoprimes(vector<int>& nums) {
         vector<int> ans;
-        int n = nums.size();
         stack<int> st;
-        for (auto& x : nums) {
-            st.push(x);
-
-            while (st.size() > 1) {
-                int left = st.top();
+        for(auto& n : nums)
+        {
+            st.push(n);
+            while(st.size()>=2)
+            {
+                int num = st.top();
                 st.pop();
-                int right = st.top();
+                int cmp = st.top();
                 st.pop();
-
-                if (gcd(left, right) > 1) {
-                    st.push(lcm(left, right));
-                } else {
-                    st.push(right);
-                    st.push(left);
+                if(gcd(num,cmp)>1)
+                {
+                    st.push(lcm(num,cmp));
+                }
+                else{
+                    st.push(cmp);
+                    st.push(num);
                     break;
                 }
             }
         }
-
-        while (!st.empty()) {
-            int num = st.top();
+        while(!st.empty()){
+            ans.push_back(st.top());
             st.pop();
-            ans.push_back(num);
         }
         reverse(ans.begin(),ans.end());
         return ans;
     }
 };
-auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
