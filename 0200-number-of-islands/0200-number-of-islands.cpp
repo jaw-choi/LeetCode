d0d15cca05a70bdc1f802d89126576d4dfb48342
jaewoo.choi <wod6983@gmail.com>
2025-10-13 14:55:07 +0900

    Time: 31 ms (31.5%), Space: 19.6 MB (38.98%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> dirY = {-1,1,0,0};
+    vector<int> dirX = {0,0,-1,1};
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        //int cnt = 0;
+        int ans = 0;
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        queue<pair<int,int>> q;
+        
+        for(int y=0;y<n;y++)
+        {
+            for(int x=0;x<m;x++)
+            {
+                if(grid[y][x]=='0') continue;
+                if(visited[y][x]==1) continue;
+                visited[y][x] = 1;
+                q.push({y,x});
+                //cnt = 0;
+                while(!q.empty())
+                {
+                    auto [r,c] = q.front();
+                    q.pop();
+                    //cnt++;
+                    
+                    for(int i=0;i<4;i++)
+                    {
+                        int ny = r + dirY[i];
+                        int nx = c + dirX[i];
+                        if(ny<0 || ny>=n || nx <0 || nx>=m) continue;
+                        if(grid[ny][nx]=='0') continue;
+                        if(visited[ny][nx]) continue;
+                        visited[ny][nx]=1;
+                        
+                        q.push({ny,nx});
+                    }
+                }
+                ans++;
+                //cout << cnt << endl;
+                //ans = max(ans,cnt);
+            }
+        }
+        return ans;
+    }
+};
