adaacde3d0aaddce775689e36f1cd2c59b8fd834
jaewoo.choi <wod6983@gmail.com>
2025-10-23 15:26:07 +0900

    Time: 0 ms (100%), Space: 8.3 MB (95.79%) - LeetHub

--- 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -13,7 +13,12 @@ public:
         
         while(s.size()!=2)
         {
-            s = operation(s);
+            for(int i=0;i<s.size()-1;i++)
+            {
+                s[i] = (((s[i]-'0') + (s[i+1]-'0'))%10) + '0';
+            }
+            s.pop_back();
+            //s = operation(s);
         }
         return s[0]==s[1];
     }
