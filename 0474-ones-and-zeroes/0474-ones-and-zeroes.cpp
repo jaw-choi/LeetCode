133297b6089585409c9feb742a6252e8ed089ef8
jaewoo.choi <wod6983@gmail.com>
2025-11-21 18:13:21 +0900

    Time: 48 ms (68.99%), Space: 13.9 MB (57.31%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
+    static int findMaxForm(vector<string>& strs, int m, int n) {
         vector<vector<int> > dp(m+1, vector<int>(n+1, 0));
         for(auto& str : strs)
         {
