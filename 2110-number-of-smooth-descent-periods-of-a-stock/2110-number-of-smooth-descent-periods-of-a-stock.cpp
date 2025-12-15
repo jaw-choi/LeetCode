de97a2c53713c67c53c66cbec56f44707f32def5
jaewoo.choi <wod6983@gmail.com>
2025-12-15 17:36:10 +0900

    Time: 5 ms (17.47%), Space: 107.1 MB (24.91%) - LeetHub

--- 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
-    long long helper(long long n)
-    {
-        return (n*(n+1) / 2) - n;
-    }
+    long long helper(long long n) { return (n * (n + 1) / 2) - n; }
     long long getDescentPeriods(vector<int>& prices) {
         int left = 0, right = 1;
         int n = prices.size();
         long long ans = n;
         int cnt = 1;
-        if(n==1)
+        if (n == 1)
             return 1;
-        while(right < n)
-        {
-            if(prices[left] - cnt == prices[right])
-            {
+        while (right < n) {
+            if (prices[left] - cnt == prices[right]) {
                 right++;
                 cnt++;
-            }
-            else
-            {
-                if(cnt!=1)
-                {
+            } else {
+                if (cnt != 1) {
                     ans += helper(right - left);
-                    //cout << left << " " << right <<endl;
+                    // cout << left << " " << right <<endl;
                     left = right;
                     right = left + 1;
                     cnt = 1;
-                }
-                else{
+                } else {
                     left++;
-                    right = left+1;
+                    right = left + 1;
                 }
             }
         }
-                if(cnt!=1)
-                {
-                    ans += helper(right - left);
-                    //cout << left << " " << right <<endl;
-                }
+        if (cnt != 1) {
+            ans += helper(right - left);
+            // cout << left << " " << right <<endl;
+        }
         return ans;
     }
 };
