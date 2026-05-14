97ac37b7b3cf8daeca4a82bdef9a02507d07138d
jaewoo.choi <wod6983@gmail.com>
2026-05-14 09:10:59 +0900

    Time: 27 ms (91.15%), Space: 97.3 MB (66.62%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,45 +1,26 @@
-bool cmp1(const vector<int>& a, const vector<int>& b) 
-{
-     return a[1] > b[1];
-}
-
-bool cmp2(const vector<int>& a, const vector<int>& b) 
-{
-     return abs(a[1] - a[0]) > abs(b[1] - b[0]); 
-}
-
 
 class Solution {
 public:
     int minimumEffort(vector<vector<int>>& tasks) {
-        int ans = 0;
-        sort(tasks.begin(), tasks.end(), cmp1);
-        ans = tasks[0][1];
-        sort(tasks.begin(), tasks.end(), cmp2);
-        int tmp = ans;
-        int cnt = 0;
-        for (int i=0;i<tasks.size();i++)
+        sort(tasks.begin(),tasks.end(),[](const vector<int>& a, const vector<int>& b){
+            return a[1]-a[0] > b[1]-b[0];
+        });
+        int energy = 0;
+        int current = 0;
+
+        for(auto& task : tasks)
         {
+            int actual = task[0];
+            int minimum = task[1];
 
-            if(i==tasks.size()-1)
+            if(current < minimum)
             {
-                cnt += tasks[i][1];
-                break;
+                energy += (minimum - current);
+                current = minimum;
             }
-            cnt += tasks[i][0];
-            if(tmp < tasks[i][1])
-            {
-                ans += abs(tmp-tasks[i][1]);
-                tmp = tasks[i][1];
-            }
-            tmp -= tasks[i][0];
+
+            current = current - actual;
         }
-
-        return max(ans,cnt);
+        return energy;
     }
 };
-// MAX((모든 r값 합 - 시그마(R-L)) , 모든 L값)
-// 57-(36),21 = 21
-// 6,5,4,3,2,7 = 27
-// 10,10,8,2,3 = 
-// [10,12],[10,11],[8,9],[2,4],[[1,3]]
