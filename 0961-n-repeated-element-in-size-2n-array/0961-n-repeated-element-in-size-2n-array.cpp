49100c07b8d3af088219904ff4357ce868e5e2d8
jaewoo.choi <wod6983@gmail.com>
2026-06-01 08:58:56 +0900

    Time: 0 ms (100%), Space: 28.6 MB (58.24%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        unordered_map<int,int> mp;
+        unordered_set<int> s;
         for(auto& n : nums)
-            mp[n]++;
-        for(auto& m : mp){
-            if(m.second == nums.size()/2)
-                return m.first;
+        {
+            if(s.count(n))
+                return n;
+            s.insert(n);       
         }
+
         return 0;
     }
 };
